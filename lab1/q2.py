5684496bb3076d7e5851594b5568ac054a1e9319
a9862 <shanta.khanal09@gmail.com>
2021-02-24 11:29:09 +0545

    name

--- lab1/q2.py
+++ lab1/q2.py
@@ -1,5 +1,5 @@
 #program that read the lengthh of base and height of right angle trangle
 length= int(input('enter a Length: '))
 breath= int(input('enter a breath: '))
-area = length *breath
+area = (length *breath)/2
 print(area)
