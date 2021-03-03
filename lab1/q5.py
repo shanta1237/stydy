9dceb49ec7718f5ba8e123690eefb8776e802425
a9862 <shanta.khanal09@gmail.com>
2021-03-03 11:58:54 +0545

    initial commit

---
+++ lab1/q5.py
@@ -0,0 +1,7 @@
+# A school decides to replace the desk in 3 class =room, each desk sits two students. give the number of student in each class,print the smallest possible nhumber of desk that can be purchased.
+i = int(input('enter a number of student in classi: '))
+j = int(input('enter a number of student in classj: '))
+k = int(input('enter a number of student in classk: '))
+print(i//2 + i % 2)
+print(j//2 + j % 2)
+print(k//2 + k% 2)
